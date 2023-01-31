97566cd66062c5c6615752f2f98cc641416ee9c1
PowerOleg <geraskin.oleg@mail.ru>
2023-01-31 20:57:54 +0500

    TodoServer. ServerLogic. 2.2-snapshot

---
+++ src/main/java/ru/netology/javacore/ServerLogic.java
@@ -0,0 +1,5 @@
+package ru.netology.javacore;
+
+public class ServerLogic {
+
+}
--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -5,41 +5,51 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Scanner;                                               //???
 
 public class TodoServer {
-    protected String host = "127.0.0.1";
     protected int port;
     protected Todos todos;
+    protected ServerLogic serverLogic;
 
     public TodoServer(int port, Todos todos) {
     this.port = port;
     this.todos = todos;
+    serverLogic = new ServerLogic();
     }
 
-    public void start() /*throws IOException*/ {
-        try (ServerSocket serverSocket = new ServerSocket()) {
-            serverSocket.accept();
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Starting server at " + port + "...");
             while (true) {
-                try (Socket socket = new Socket(host, port); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                try (Socket socket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                      BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-
+                    System.out.println("1");                                                                //d
                     String clientRequest = in.readLine();
                     System.out.println(clientRequest);
+                    System.out.println("2");                                                                //d
 
-                    out.write(clientRequest);
+                    //serverLogic определяющий ADD or REMOVE
+
+                    System.out.println("3");                                                                //d
+                    /* ! */                    String response = todos.getAllTasks();
+                    out.write(response);
                     out.newLine();
                     out.flush();
                 }
-
-
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
 
-//        System.out.println("Starting server at " + port + "...");
 
+
+    }
+
+    public ServerLogic getServerLogic() {
+        return serverLogic;
+    }
+
+    public void setServerLogic(ServerLogic serverLogic) {
+        this.serverLogic = serverLogic;
     }
 }
