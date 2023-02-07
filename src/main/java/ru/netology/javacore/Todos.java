dd280ef87a771d7a8d007188c9aa13c8f9d4dcc3
PowerOleg <geraskin.oleg@mail.ru>
2023-02-07 20:40:18 +0500

    Todos. public static final MAX_TASKS. 4.1-snapshot

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Todos {
     protected Deque<Task> tasks = new ArrayDeque<>();
-    protected int MAX_TASKS = 7;
+    public static final int MAX_TASKS = 7;
 
     public boolean addTask(String task) {
         int count = 0;
@@ -42,13 +42,5 @@ public class Todos {
     public String getAllTasks() {
         return tasks.stream().filter(n -> !n.isDeleted()).map(Task::getTask)
                 .sorted().distinct().collect(Collectors.joining(" "));
-    }
-
-    public int getMAX_TASKS() {
-        return MAX_TASKS;
-    }
-
-    public void setMAX_TASKS(int maxTasks) {
-        this.MAX_TASKS = maxTasks;
     }
 }
